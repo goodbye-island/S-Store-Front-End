dde9fdffe9216d1c3190394b6fd1cd43a306b17b
Isaac Park <isaac.a.park@gmail.com>
2016-10-13 14:57:58 -0400

    Properly List Links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,19 +2,23 @@ import * as React from "react";
 
 export interface NavbarProps {
     title: string|JSX.Element,
-    items: (string|JSX.Element)[]
+    items: (string|JSX.Element)[],
+    height?: number|string
 }
 
 export class Navbar extends React.Component<NavbarProps, {}> {
     render() {
+        let height = this.props.height ? this.props.height:60;
         let items = this.props.items.map(item => {
-            return <div style={{display: "inline-block", padding: 15, boxSizing: "content-box"}}>{item}</div>
+            return <li style={{display: "block", paddingRight: 15, float: "left", height: height}}>{item}</li>
         })
-        return  <div style={{width: "100%", height: 60, background: "#002664", color: "white"}} className="navbar">
-                    <a href="/" style={{display: "inline-block"}}>
+        return  <div style={{width: "100%", height: height, background: "#002664", color: "white"}} className="navbar">
+                    <a href="/" style={{display: "inline-block", height: height}}>
                         {this.props.title}
                     </a>
+                    <ul style={{display: "block", float: "right", height: height}}>
                     {items}
+                    </ul>
                 </div>
     }
 }
