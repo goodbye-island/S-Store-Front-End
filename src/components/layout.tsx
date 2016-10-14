ed2ca98e0d0dc606332b8521e9ad6125a5ea320d
Isaac Park <isaac.a.park@gmail.com>
2016-10-13 20:54:08 -0400

    Padding

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,9 @@ export class Layout extends React.Component<LayoutProps, {}> {
      render() {
          return <div>
                     <Navbar title={<img src="logo.svg" height="35" style={{padding: 2.5}}/>} height="40" items={[<span style={{padding: 7, fontSize: 30}}>Nothing yet</span>]}/>
-                    {this.props.children}
+                    <div style={{"margin": 30}}>
+                        {this.props.children}
+                    </div>
                 </div>
     }
 }
