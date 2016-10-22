6aa03c5443b759ab5538aca6d1b1df5e27e393da
Isaac Park <isaac.a.park@gmail.com>
2016-10-22 13:15:04 -0400

    Remove Old Static Classes

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -7,14 +7,6 @@ export interface SearchProps {}
 export class Search extends React.Component<SearchProps, {title: string}> {
     state: {title: string} = {title: undefined}
     render() {
-        let classes: {title: string, professors: string[]}[] = [
-            {title: "hi", professors: ["Isaac Park"]},
-            {title: "Capstone is great", professors: ["Test"]},
-            {title: "CS 1", professors: ["Facebook"]},
-            {title: "CS 2", professors: ["stackoverflow"]},
-            {title: "CS 3", professors: ["reddit"]},
-
-        ]
         return  <div>
                     <h1>Courses</h1>
                     <div>
