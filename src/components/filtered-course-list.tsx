fcba3b7d1caa1aa1396056941d8e31b4745e8fc1
Isaac Park <isaac.a.park@gmail.com>
2016-10-22 15:38:20 -0400

    Use Filter  Function

--- src/components/filtered-course-list.tsx
+++ src/components/filtered-course-list.tsx
@@ -3,15 +3,14 @@ import * as React from "react";
 import { connect } from 'react-redux'
 import { State } from '../reducers'
 import { Filter } from '../filter'
+import { filter } from '../filter'
 
 import {CourseList} from "./course-list"
 
 
 const mapStateToProps = (state: State) => {
   return {
-      classes: state.courses.filter( (c, i) => {
-             return state.filter.title == undefined || c.title.toLowerCase().indexOf(state.filter.title.toLowerCase()) >= 0;
-         })
+      classes: state.courses.filter(filter(state))
   }
 }
 const mapDispatchToProps = (dispatch: any) => {
