660bff2f137ba65a1736073345f125c3e1054906
Isaac Park <isaac.a.park@gmail.com>
2016-11-07 15:04:42 -0500

    Animation On Hover Title

--- src/components/course-summary.tsx
+++ src/components/course-summary.tsx
@@ -11,12 +11,13 @@ export interface CourseSummaryProps {
 export class CourseSummary extends React.Component<CourseSummaryProps, {}> {
     state: {expanded: boolean} = {expanded: false};
     render() {
-        return  <div className="course">
+        return  <div className={"course" + (this.state.expanded?" expanded":"")}>
                     <h1 onClick={ () => this.setState({"expanded": !this.state.expanded}) }>
-                        <span className="title">{this.props.course.title} </span>
                         <span className="department-acronym"><Department departmentID={this.props.course.department}/></span>
-                         
+                        <span> </span>
                         <span className="course-number">{this.props.course.course}</span>
+                        <span> </span>
+                        <span className="title">{this.props.course.title} </span>
                     </h1>
                     <Toggle expanded={this.state.expanded}>
                         <div className="description">
