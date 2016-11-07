79191bbfed9adcc313f7d015d9d7cf2a0cfb2497
Isaac Park <isaac.a.park@gmail.com>
2016-11-07 15:20:33 -0500

    More Abstract Input

--- src/components/utilities/int-input.tsx
+++ src/components/utilities/int-input.tsx
@@ -1,12 +1,9 @@
 import * as React from "react";
-import {InputProps} from "./input"
+import {Input,  InputProps} from "./input"
 export interface IntInputProps extends InputProps{
     onChange: (value: number) => void,
 }
 
 export function IntInput(props: IntInputProps) {
-    return  <div className={props.class +" float-label"}>
-                <input required value={props.value} id={props.id} onChange={(e: any) => props.onChange(parseInt(e.target.value))} />
-                <label> {props.label} </label>
-            </div>
+    return <Input {...props} onChange={(e: any) => props.onChange(parseInt(e.target.value))}/>
 }
