5fc4db17a604e5bf31e355c2540b8aa1dfa22a25
Isaac Park <isaac.a.park@gmail.com>
2016-11-08 00:23:43 -0500

    Remove Unused Filters

--- src/components/class-filter-input.tsx
+++ src/components/class-filter-input.tsx
@@ -18,10 +18,13 @@ export class ClassFilterInput extends React.Component<ClassFilterInputProps, {}>
                         this.setState({title: title});
                         this.props.onChange(objectAssign({}, this.state, {title: title}));
                     }}/>
-                    <Input label="Keyword" onChange={keyword => {
-                        this.setState({keyword: keyword});
-                        this.props.onChange(objectAssign({}, this.state, {keyword: keyword}));
-                    }}/>
+                    <br/>
+                    <DropdownStringInput label="Department" onChange = {
+                        (id) => {
+                            this.setState({department: id});
+                            this.props.onChange(objectAssign({}, this.state, {department: id}));
+                        }
+                    } options = {Object.keys(this.props.departments).map(id => {return {id: parseInt(id), value: this.props.departments[parseInt(id)].title}})}/>
                     <IntInput label="Course Num" onChange={course => {
                         if (isNaN(course)) {
                             course = undefined;
@@ -29,29 +32,12 @@ export class ClassFilterInput extends React.Component<ClassFilterInputProps, {}>
                         this.setState({course: course});
                         this.props.onChange(objectAssign({}, this.state, {course: course}));
                     }}/>
-                    <DropdownStringInput label="Department" onChange = {
-                        (id) => {
-                            this.setState({department: id});
-                            this.props.onChange(objectAssign({}, this.state, {department: id}));
-                        }
-                    } options = {Object.keys(this.props.departments).map(id => {return {id: parseInt(id), value: this.props.departments[parseInt(id)].title}})}/>
-                    <IntInput label="Semester" onChange={semester => {
-                        if (isNaN(semester)) {
-                            semester = undefined;
-                        }
-                        this.setState({semester: semester});
-                        this.props.onChange(objectAssign({}, this.state, {semester: semester}));
-                    }}/>
                     <IntInput label="Section" onChange={section => {
                         if (isNaN(section)) {
                             section = undefined;
                         }
                         this.setState({section: section});
                         this.props.onChange(objectAssign({}, this.state, {section: section}));
-                    }}/>
-                    <Input label="Teacher" onChange={teacher => {
-                        this.setState({teacher: teacher});
-                        this.props.onChange(objectAssign({}, this.state, {teacher: teacher}));
                     }}/>
                 </div>
     }
