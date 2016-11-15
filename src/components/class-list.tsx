88bb997d62310364775acc4d7c7f21db90106b90
Isaac Park <isaac.a.park@gmail.com>
2016-11-15 10:14:58 -0500

    Remove Class Add From Class List

--- src/components/class-list.tsx
+++ src/components/class-list.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { ClassSummary } from "./class-summary"
 import {ClassXCourse} from "../class"
-import { ClassAdd } from "./class-add"
 
 interface ClassString extends ClassXCourse {
     teacher_string: string,
@@ -30,7 +29,6 @@ export class ClassList extends React.Component<ClassListProps, {}> {
                     </thead>
                     <tbody>
                     {classes}
-                    <ClassAdd/>
                     </tbody>
                 </table>
     }
