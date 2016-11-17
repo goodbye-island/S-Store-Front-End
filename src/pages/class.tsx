a13cf21e23a528f293c022a44c3a84aed9299ae7
Isaac Park <isaac.a.park@gmail.com>
2016-11-17 09:57:40 -0500

    Class Page

--- src/pages/class.tsx
+++ src/pages/class.tsx
@@ -1,20 +1,39 @@
 import * as React from "react";
-import {Class} from "../class"
+import {ClassXCourse} from "../class"
 import {ClassSummary} from "../components/class-summary"
 
 import { connect } from 'react-redux'
 import { State } from '../reducers'
+import { newFilter } from '../actions'
+import config from '../config'
 
 export interface ClassProps {
     params: {CRN: string}
 }
 
+export interface ConnectedClass {
+    class_: ClassXCourse,
+    CRN: number,
+    dispatch: (action: any) => void
+}
 
-export const ClassView = connect( (state: State, props: ClassProps) => ({class_: state.classes.find( c=>c.CRN == parseFloat(props.params.CRN))}))
-(class extends React.Component<{class_: Class}, {}> {
+export const ClassView = connect( (state: State, props: ClassProps) => ({class_: state.classes.find( c=>c.CRN == parseInt(props.params.CRN)), CRN: parseInt(props.params.CRN)}))
+(class extends React.Component<ConnectedClass, {}> {
+    static contextTypes = {
+        store: React.PropTypes.object
+    }
+    constructor(props: ConnectedClass) {
+        super(props);
+        console.log(props.dispatch)
+        props.dispatch(newFilter({"CRN": props.CRN}, config.api)) 
+    }
     render() {
+        if (!this.props.class_) {
+            return <div> </div>
+        }
+        console.log(this.props.class_, this.context, this.props.CRN)
         return  <div>
-                    
+                    <ClassSummary class_={this.props.class_} />
                 </div>
     }
 })
