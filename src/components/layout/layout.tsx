afcb16d0e3827e5cfb68b201632f34b979e6fdc0
Isaac Park <isaac.a.park@gmail.com>
2016-12-03 18:49:08 -0500

    Set Height in The Style Too

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -8,8 +8,8 @@ export interface LayoutProps {
 export class Layout extends React.Component<LayoutProps, {}> {
      render() {
          return <div>
-                    <Navbar title={<img src="/logo.svg" height="35"
-                            style={{padding: 2.5}}/>}
+                    <Navbar title={<img src="/logo.svg"
+                            style={{padding: 2.5, height: "35px"}}/>}
                             height="40" 
                             items={[<Link style={{padding: 7, fontSize: 30, color: "white"}} activeStyle={{color: "red"}} to="/about">About</Link>,
                                     <Link style={{padding: 7, fontSize: 30, color: "white"}} activeStyle={{color: "red"}} to="/login">Login</Link>]}
