60c6e295607e6091c679eeef3b8353ce40c99447
Isaac Park <isaac.a.park@gmail.com>
2016-12-06 01:04:15 -0500

    Add Link to Add Course

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -11,8 +11,8 @@ export class Layout extends React.Component<LayoutProps, {}> {
                     <Navbar title={<img src="/logo.svg"
                             style={{padding: 2.5, height: "35px"}}/>}
                             height="40" 
-                            items={[<Link style={{padding: 7, fontSize: 30, color: "white"}} activeStyle={{color: "red"}} to="/about">About</Link>,
-                                    <Link style={{padding: 7, fontSize: 30, color: "white"}} activeStyle={{color: "red"}} to="/login">Login</Link>]}
+                            items={[<Link style={{padding: 7, fontSize: 30, color: "white"}} activeStyle={{fontWeight: "bold"}} to="/about">About</Link>,
+                                    <Link style={{padding: 7, fontSize: 30, color: "white"}} activeStyle={{fontWeight: "bold"}} to="/course/add/">Add Course</Link>]}
                             />
                     <div style={{minHeight: "100%", "margin": "0 auto"}} className="content">
                         {this.props.children}
