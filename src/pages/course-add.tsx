cc387b432181f7a2badad860947f08fb9abb83b8
Isaac Park <isaac.a.park@gmail.com>
2016-12-05 22:17:54 -0500

    Added Error Handling

--- src/pages/course-add.tsx
+++ src/pages/course-add.tsx
@@ -3,6 +3,7 @@ import { State } from '../control/reducers'
 import { User, Roles } from '../control/user'
 import * as React from "react";
 import {Course, addCourseRequest} from "../control/class"
+
 import {DropdownStringInput} from '../components/utilities/dropdown-string-input'
 import {Input} from '../components/utilities/input'
 import {IntInput} from '../components/utilities/int-input'
@@ -11,19 +12,32 @@ import {DepartmentDropdown} from '../components/department-dropdown'
 
 export interface CourseAdd {
     user: User,
+    departments: { [id: number]: {title: string, abbreviation: string}}
 }
 
-export const CourseAdd = connect( (state: State) => ({user: state.user}))
+interface CourseAddState extends Course{
+    error: string,
+    result?: JSX.Element
+}
+
+export const CourseAdd = connect( (state: State) => ({user: state.user, departments: state.departments}))
 (class extends React.Component<CourseAdd, {}> {
-    state: Course = {title: "", course: undefined, department: undefined, keyword: "", description: "", classes: []}
+    state: CourseAddState = {title: "", course: undefined, department: undefined, keyword: "", description: "", classes: [], error: ""}
     render() {
         return <form className="" onSubmit={
             e => {
                 e.preventDefault()
                 addCourseRequest(this.state)
-                .then(e => console.log(e))
+                .then(result => {
+                    if (result) {
+                        this.setState({error: "", result: <a href={location.protocol + "//" + location.host + "/class/add/"+this.props.departments[this.state.department].abbreviation +"/"+this.state.course}>Successful: Add a class</a>});
+                    } else {
+                        this.setState({error: "Something went wrong", result: null})
+                    }
+                });
             }
         }>
+            <h1 style={{textAlign: "center", fontSize: "60px"}}>Add a Course</h1>
             <Input value={this.state.title} label="Title" onChange={value => this.setState({title: value})} />
             <DepartmentDropdown onChange={value => this.setState({department: value})} label="Department"/>
             <IntInput label="Course #" onChange={value => this.setState({course: value})} /><br/>
@@ -32,7 +46,13 @@ export const CourseAdd = connect( (state: State) => ({user: state.user}))
                 value={this.state.description}
                 label="Description"
                 onChange={(value: any) => this.setState({description: value.target.value})} />
-            <input type="submit"/> 
+            <input type="submit"/>
+            <div className="error">
+                {this.state.error}
+            </div>
+            <div className="result">
+                {this.state.result}
+            </div>
         </form>
     }
 })
